5cd22d223ebc2f31c1dd8d92be0981426e5c6124
Tony Rim <duc_thinh_03@yahoo.com>
2020-04-23 20:52:19 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -17,7 +17,6 @@ def long_planeteer_calls(array)
 end
 
 def find_the_cheese(array)
-  cheese_types = ["cheddar", "gouda", "camembert"]
   array.find? do |cheese|
         cheese == "cheddar" || cheese == "gouda" || cheese == "camenbert"
 end
